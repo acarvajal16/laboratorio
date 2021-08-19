eae94d5d31d6530d177a3acd45582c23c02e9a80
acarvajal16 <maria.carvajal.2020@upb.edu.co>
2021-08-19 14:36:58 -0500

    "Interfaz realizada"

---
+++ src/main/java/classes/IListaEnlazada.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Administrador
+ */
+public interface IListaEnlazada {
+    
+    public void agregar (Nodo n);
+    public Nodo buscarPorIndice(int indice);
+    public void eliminarPorIndice(int indice);
+    public Nodo obtenerPrimero();
+    public Nodo obtenerUltimo();
+    public void eliminarPrimero();
+    public void eliminarUltimo();
+    public void vaciar();
+    
+    }
+    
